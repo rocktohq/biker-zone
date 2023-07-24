c6df56cd27c5a7bab755b212d716a2c2116614be
rocktohq <itzmonir@gmail.com>
2023-07-24 22:22:04 +0600

    Initial Update

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'bike-primary': '#E76F51',
+                'bike-primary-bg': 'rgba(231, 111, 81, 0.10)',
+            }
+        }
+    }
+}
